da707ee16ceea0e5e726c4c064d01d5aae312d58
CoitoSalvaje <laboratorio@UPOAULA10602>
2024-11-22 08:11:50 -0500

    Interfaz aplicada

---
+++ PagesMaui/Interfaces/IEstudianteRepository.cs
@@ -0,0 +1,18 @@
+﻿using PagesMaui.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagesMaui.Interfaces
+{
+    public interface IEstudianteRepository
+    {
+        List<IEstudianteRepository> DevuelveListadoEstudiante();
+        Estudiante GetEstudiante(int id);
+        Boolean CrearEstudiante(Estudiante estudiante);
+        Boolean ActualizarEstudiante(Estudiante estudiante);
+        Boolean EliminarEstudiante(int id);
+    }
+}
---
+++ PagesMaui/Repositories/EstudianteRepository.cs
@@ -0,0 +1,48 @@
+﻿using PagesMaui.Interfaces;
+using PagesMaui.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagesMaui.Repositories
+{
+    public class EstudianteRepository : IEstudianteRepository
+    {
+        public bool ActualizarEstudiante(Estudiante estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CrearEstudiante(Estudiante estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Estudiante> DevuelveListadoEstudiante()
+        {
+           List<Estudiante> estudiantes= new List<Estudiante>();
+            estudiantes.Add(GetEstudiante(1));
+            estudiantes.Add(GetEstudiante(2));
+            estudiantes.Add(GetEstudiante(3));
+            return estudiantes;
+           
+        }
+
+        public bool EliminarEstudiante(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Estudiante GetEstudiante(int id)
+        {
+            return new Estudiante
+            {
+                Id = id,
+                Name = "Camily",
+                Degree = "Ing Software"
+            };
+        }
+    }
+}
