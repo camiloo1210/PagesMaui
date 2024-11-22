faeb9b09a7f1e2ac508dafff94dd96b6d4c2383a
CoitoSalvaje <laboratorio@UPOAULA10602>
2024-11-22 07:53:28 -0500

    Creacion carpetas repositorios models

---
+++ PagesMaui/Models/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagesMaui.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Degree { get; set; }
+
+    }
+}
